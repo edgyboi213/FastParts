49f5dc29e24ec31050152eed3430432f6085bee4
root <root@serverjoil.novalocal>
2026-06-21 19:58:39 +0000

    updated logic for server deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Category, Part, User, CartItem, Order } from '../types';
 
-const API_BASE_URL = 'http://localhost:5103/api'; // Standard local ASP.NET URL, user can change in .env if needed
+const API_BASE_URL = 'https://edgyboi.x1roko.online/api'; // Standard local ASP.NET URL, user can change in .env if needed
 
 const api = axios.create({
   baseURL: API_BASE_URL,
